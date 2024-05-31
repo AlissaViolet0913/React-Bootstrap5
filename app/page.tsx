78ef463fc5c0bde86c709c067e3c2c680f84af2e
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2024-05-31 17:53:08 +0900

    ヘッダー完成

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import styles from "./style.module.css";
+import { Button, Stack } from 'react-bootstrap';
 
-export default function Home() {
+function BasicExample() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+    {/* navbar section */}
+    <Navbar expand="lg" className="bg-dark">
+      <Container>
+        <Navbar.Brand href="#home" className={`${styles.bootstrap} text-white pt-1`}>Bootstrap5</Navbar.Brand>
+        <Navbar.Toggle  />
+        <Navbar.Collapse id="basic-navbar-nav">
+          {/* ms-auto: 要素右寄せ */}
+          {/* me-auto: 要素左寄せ */}
+          <Nav className="ms-auto">
+              <Nav.Link href="#home" className={styles.navLink}>Home</Nav.Link>
+              <Nav.Link href="#link" className={styles.navLink}>Link</Nav.Link>
+              <Nav.Link href="#features" className={styles.navLink}>Features</Nav.Link>
+              <Nav.Link href="#projects" className={styles.navLink}>Projects</Nav.Link>
+              <Nav.Link href="#team" className={styles.navLink}>Team</Nav.Link>
+              <Nav.Link href="#blog" className={styles.navLink}>Blog</Nav.Link>
+              <Nav.Link href="contract" className={styles.navLink}>Contract</Nav.Link>
+              <Button variant="primary">Join Us</Button>{' '}
+            </Nav>
+        </Navbar.Collapse> 
+      </Container>
+    </Navbar>
+
+    {/* hero section */}
+    {/* div.heroでショートカット⇓ */}
+    <div className="hero" id="hero">
+      {/* container: bootstrap特有の幅指定 */}
+      <div className="container">
+        <div className="row">
+          {/* col: colam（カラム）のこと。列。 */}
+          <div className="col-lg-7">
+            <h1>Bootstarp5 Page</h1>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
+    </>
   );
 }
+
+export default BasicExample;
