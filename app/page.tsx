1ce41f955d6e547323ea2e75be81ac581c2c781f
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2024-06-01 11:37:23 +0900

    一旦保存

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { Button, Stack } from 'react-bootstrap';
 function BasicExample() {
   return (
     <>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
     {/* navbar section */}
     <Navbar expand="lg" className="bg-dark">
       <Container>
@@ -33,17 +34,52 @@ function BasicExample() {
 
     {/* hero section */}
     {/* div.heroでショートカット⇓ */}
-    <div className="hero" id="hero">
-      {/* container: bootstrap特有の幅指定 */}
+    {/* flexにしてalign-items-centerにすることで要素が縦に中央へ行く */}
+    <div className={`${styles.hero} vh-100 d-flex align-items-center`} id="hero">
+      {/* container: bootstrap特有の幅指定。レスポンシブ対応 */}
       <div className="container">
         <div className="row">
           {/* col: colam（カラム）のこと。列。 */}
-          <div className="col-lg-7">
-            <h1>Bootstarp5 Page</h1>
+          {/*7: 七分割。この数字を下げるとさらに要素が小さくなる */}
+          {/* max-auto: 左右のmarginを自動的に設ける */}
+          <div className="col-lg-7 mx-auto text-center">
+            <h1 className={`${styles.heading} display-4 text-white`}>Bootstarp5 Page</h1>
+            {/* my-3: marginを3空ける */}
+            {/* lorem15: 15文字の適当な文章を入力してくれる */}
+            <p className='text-white my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quisquam dolores facilis incidunt obcaecati illo perspiciatis minima molestiae fugiat eos. Omnis laudantium dicta esse porro ducimus, enim neque et. Libero.</p>
+            <a href="#" className="btn btn-primary me-2">Go Page</a>
+            <a href="#" className="btn btn-outline-light">Contact</a>
           </div>
         </div>
       </div>
     </div>
+
+    {/* Service section */}
+    <section id="servies" className={styles.section}>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 mx-auto text-center">
+            <h6 className={`${styles.heading} text-primary`}>SERVICES</h6>
+            <h1 className={`${styles.heading}`}>Services</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, praesentium.</p>
+          </div>
+        </div>
+
+        {/* row: 横並びにする */}
+        <div className="row">
+          <div className="col-lg-4 col-sm-6">
+            <div className={`${styles.service} ${styles.cardEffect}`}>
+              <div className={styles.iconbox}>
+              <i className='bx bxs-comment-detail' ></i>
+              </div>
+              <h5>Service</h5>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sunt expedita, dolorem natus quae eligendi.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     </>
   );
 }
